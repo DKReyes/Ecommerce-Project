8d543a9a68a570d0c809608e23f54fda7517c98b
DKReyes <dylankreyes@gmail.com>
2022-11-07 19:35:29 -0600

    Added Customer controller
    Added customer controller with index and show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
   get 'laptops/index'
   get 'laptops/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
