0954b9434b2b4d00bd791b8e77c81beb5f63b532
DKReyes <dylankreyes@gmail.com>
2022-11-23 12:10:45 -0600

    Implemented delete from cart function

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,13 +7,18 @@ class CartController < ApplicationController
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id
       laptop = Laptop.find(id)
-      flash[:notice] = " #{laptop.name} added to cart."
+      flash[:notice] = " [ #{laptop.name} ] added to cart."
     end
 
     redirect_to root_path
   end
 
   def destroy
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    laptop = Laptop.find(id)
 
+    redirect_to root_path
+    flash[:notice] = " [ #{laptop.name} ] Removed from cart."
   end
 end
