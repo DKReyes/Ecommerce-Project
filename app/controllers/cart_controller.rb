5d19aec07219ba6f1d07911e106fa127f29d5e8b
DKReyes <dylankreyes@gmail.com>
2022-11-23 11:17:16 -0600

    Added Flash notice to added item on cart.
    - Added flash notice
    - Disabled duplicate notice on multiple of same item

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,14 @@ class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-    session[:shopping_cart] << id # Pushes ID to the end of the array.
+
+    # Pushes ID to the end of the array.
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      laptop = Laptop.find(id)
+      flash[:notice] = " #{laptop.name} added to cart."
+    end
+
     redirect_to root_path
   end
 
