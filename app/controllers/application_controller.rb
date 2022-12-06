43958931e9a276f40d44178804a8637a8dfedb41
DKReyes <dylankreyes@gmail.com>
2022-12-06 11:45:15 -0600

    Added province and address to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:province_id, :email, :password, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:address, :province_id, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit( :address, :province_id, :email, :password, :password_confirmation, :current_password) }
   end
 
   private
